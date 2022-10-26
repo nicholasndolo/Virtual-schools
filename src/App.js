5ef88e59745d2ae6ece3fd8677e59e99c4854a7b
glenn-nguyo <glennnguyo1000@gmail.com>
2022-10-26 17:04:31 +0300

    fetch tryout

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import Auth from "./components/Login/Auth"
 import Home from "./pages/Home";
 import AboutUs from "./components/About-us/AboutUs";
 import Courses from "./components/Courses-section/Courses";
-import School from "./components/School/school";
+
+//import School from "./components/School/school";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
-         <Route path="/school" element={<School />} />
-       <Route path="/login" element={<Auth />} />
-    <Route path="/courses" element={<Courses />} />
-
+        <Route path="/login" element={<Auth />} />
+     <Route path="/courses" element={<Courses />} />
       </Routes>
     </BrowserRouter>
   )
