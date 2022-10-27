99a072e25eb3589e2410068a5207dfd3371bb43a
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-10-27 12:51:59 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Auth from "./components/Login/Auth"
 import Home from "./pages/Home";
 import AboutUs from "./components/About-us/AboutUs";
 import Courses from "./components/Courses-section/Courses";
-
-//import School from "./components/School/school";
+import Learn from "./components/student-page/components/Learn";
+import School from "./components/School/school";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/learn" element={<Learn />} />
       <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/login" element={<Auth />} />
