473d3ccccca11dae92066aa7f91807df24856b86
Nicholas <nicholas.nmalinda@gmail.com>
2022-11-02 17:03:41 +0300

    Created CourseList component

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,46 +1,46 @@
-// import React,{useEffect, useState} from 'react';
-// import CourseCard from './CourseCard'
+import React,{useEffect, useState} from 'react';
+import CourseCard from './CourseCard'
 
-// function CourseList() {
-//   const [courses, setCourses] = useState([])
+function CourseList() {
+  const [courses, setCourses] = useState([])
 
-//     useEffect(()=> {
-//         fetch("https://virtualschools.herokuapp.com/courses")
-//         .then((res) => res.json())
-//         .then((data) => setCourses(data)) 
-//     }, [])
+    useEffect(()=> {
+        fetch("https://virtualschools.herokuapp.com/courses")
+        .then((res) => res.json())
+        .then((data) => setCourses(data)) 
+    }, [])
     
-//     function handleAddCourse(newCourse){
-//       setCourses([...courses, newCourse]);
-//     }
+    function handleAddCourse(newCourse){
+      setCourses([...courses, newCourse]);
+    }
     
-//     function handleDeleteCourse(id){
-//       setCourses(courses.filter(course => course.id !== id))
-//     }
+    function handleDeleteCourse(id){
+      setCourses(courses.filter(course => course.id !== id))
+    }
 
-//     function handleCourseUpdate(updatedCourse){
-//       const updatedCourses =courses.map((course) => {
-//         if(course.id === updatedCourse.id){
-//           return updatedCourse
-//         }else{
-//           return course
-//         }
-//       });
-//       setCourses(updatedCourses);
-//     }
+    function handleCourseUpdate(updatedCourse){
+      const updatedCourses =courses.map((course) => {
+        if(course.id === updatedCourse.id){
+          return updatedCourse
+        }else{
+          return course
+        }
+      });
+      setCourses(updatedCourses);
+    }
 
-//    console.log(courses)
+   console.log(courses)
 
-//     let course = courses.map((course)=>(
+    let course = courses.map((course)=>(
 
-//         <CourseCard handleCourseUpdate={handleCourseUpdate} handleDeleteCourse={handleDeleteCourse} key={course.id} setCourses={setCourses} courses={courses} title={course.title} duration={course.duration} courseId={course.id} card={course.image_url}/>
-//     ))
+        <CourseCard handleCourseUpdate={handleCourseUpdate} handleDeleteCourse={handleDeleteCourse} key={course.id} setCourses={setCourses} courses={courses} title={course.title} duration={course.duration} courseId={course.id} card={course.image_url}/>
+    ))
 
-//   return (
-//     <div className="">
-//       {course}
-//     </div>
-//   );
-// }
+  return (
+    <div className="">
+      {course}
+    </div>
+  );
+}
 
-// export default CourseList;
+export default CourseList;
