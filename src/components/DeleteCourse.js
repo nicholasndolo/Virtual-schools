edaa9eed15ba3a1dd8b99f9eba8e5c3464a661f5
Nicholas <nicholas.nmalinda@gmail.com>
2022-11-02 17:05:34 +0300

    ft Created DeleteCourse component

--- src/components/DeleteCourse.js
+++ src/components/DeleteCourse.js
@@ -1,39 +1,39 @@
-// import React from 'react'
-// import { confirmAlert } from 'react-confirm-alert'
+import React from 'react'
+import { confirmAlert } from 'react-confirm-alert'
 
-// function DeleteCourse({courseId,handleDeleteCourse}) {
+function DeleteCourse({courseId,handleDeleteCourse}) {
 
-//   function handleDelete(){
-//     fetch(`https://virtualschools.herokuapp.com/courses/${courseId}`,{
-//       "method": "DELETE",
-//       "headers":{
-//         "Content-Type": "application/json"
-//       }
-//     })
-//     handleDeleteCourse(courseId)
-//   }
+  function handleDelete(){
+    fetch(`https://virtualschools.herokuapp.com/courses/${courseId}`,{
+      "method": "DELETE",
+      "headers":{
+        "Content-Type": "application/json"
+      }
+    })
+    handleDeleteCourse(courseId)
+  }
 
-//   function submit(){
-// 		confirmAlert({
-// 			title: "Delete this course",
-// 			message:"Data will be deleted parmanently. Press ok to continue",
-// 			buttons: [
-// 				{
-// 					label: "Ok",
-// 					onClick : ()=> handleDelete()
-// 				},
-// 				{
-// 					label: "No",
-// 					onClick: () => null
-// 				}
-// 			]
-// 		})
-// 	}
-//   return (
-//     <div>
-//       <button onClick={submit} id={courseId} className='comment'>Delete</button>
-//     </div>
-//   )
-// }
+  function submit(){
+		confirmAlert({
+			title: "Delete this course",
+			message:"Data will be deleted parmanently. Press ok to continue",
+			buttons: [
+				{
+					label: "Ok",
+					onClick : ()=> handleDelete()
+				},
+				{
+					label: "No",
+					onClick: () => null
+				}
+			]
+		})
+	}
+  return (
+    <div>
+      <button onClick={submit} id={courseId} className='comment'>Delete</button>
+    </div>
+  )
+}
 
-// export default DeleteCourse
+export default DeleteCourse
