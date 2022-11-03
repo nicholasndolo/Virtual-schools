355d48945a099bd2212acf0145733c28be189a17
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-11-03 23:50:37 +0300

    erase commented code

--- src/components/student-page/components/Resources.js
+++ src/components/student-page/components/Resources.js
@@ -10,8 +10,6 @@ const Resources = () => {
     const cardInfo = [
         {image:"https://media.geeksforgeeks.org/wp-content/uploads/20190709181948/Python-Crash-Course.jpg", title:"Newdoc", text:"welcome"},
         {image:"https://miro.medium.com/max/1200/1*qjzRVOyNL_AP_ZfRT0OfOw.jpeg", title:"Setbook", text:"nice book"},
-        // {image:"https://media.geeksforgeeks.org/wp-content/uploads/20190709181948/Python-Crash-Course.jpg", title:"Newdoc", text:"welcome"},
-        // {image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiJ3zc7UKIL1cobrnYTez8e_2no-OzMwrv0A&usqp=CAU", title:"Python", text:"understand"}
     ]; 
 
     const renderCard = (card, index) => {
@@ -40,7 +38,7 @@ const Resources = () => {
   return (
       <div className='res-nav'>
 
-        <Navbar className='navigation' bg="light" variant="light">
+        <Navbar className='navig' bg="light" variant="light">
         <Container>
         <Navbar.Brand href="#home" >
             <i className='bi bi-house' /> <span className='ms-1 d-none d-sm-inline'>Home</span>
