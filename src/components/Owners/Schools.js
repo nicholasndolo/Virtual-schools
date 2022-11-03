1ba849c4068385361af687beca96ee16524e8089
glenn-nguyo <glennnguyo1000@gmail.com>
2022-11-03 13:46:50 +0300

    commit

--- src/components/Owners/Schools.js
+++ src/components/Owners/Schools.js
@@ -1,8 +1,8 @@
 import Card from 'react-bootstrap/Card';
-import './src/Owner.css'
+import './Owner.css'
 function School() {
   return (
-    <Card style={{ width: '21rem', margin:'25px 25px'}}>
+    <Card style={{ width: '21rem', margin:'25px'}} className= "owners">
       <Card.Img variant="top" src="https://images.unsplash.com/photo-1509062522246-3755977927d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1232&q=80" />
       <Card.Body>
         <Card.Title>School</Card.Title>
